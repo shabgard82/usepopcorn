56b04da34a987ed422acf31f6f721eeec4432cfc
shabgard82 <pedramaliyari5@gmail.com>
2024-03-06 11:25:28 +0330

    changed

--- src/components/NumResault.jsx
+++ src/components/NumResault.jsx
@@ -1,6 +1,4 @@
 const NumResault = ({ movies }) => {
-  console.log(movies);
-  console.log(movies.length);
   return (
     <p className="num-results">
       Found <strong> {movies.movies.length} </strong>resaults
